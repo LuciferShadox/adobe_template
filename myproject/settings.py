4c27f8f9deddfb407d7253c8559c0737877e5d65
Rahul <>
2025-03-18 08:23:33 +0100

    Added logo

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^@w8&v(59r4%m5no=1rtow(6)jg72hw*oiy4ga*5j@)nw(jw6+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rahulram.pythonanywhere.com']
 
 
 # Application definition
